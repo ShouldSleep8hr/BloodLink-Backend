9544aaeecaa00543816ffcc0f99b9911666b4f49
ShouldSleep8hr <pearanuban@gmail.com>
2025-02-05 17:38:59 +0700

    fix setting for gcs again remove json.load

--- bloodlink/settings.py
+++ bloodlink/settings.py
@@ -276,8 +276,8 @@ SIMPLE_JWT = {
 GS_BUCKET_NAME = os.getenv('GS_BUCKET_NAME', 'default-bucket-name')
 
 credentials_json = os.getenv('GS_CREDENTIALS')
-credentials_json = credentials_json.replace('\\n', '\n')
-credentials_info = json.loads(credentials_json)
-GS_CREDENTIALS = service_account.Credentials.from_service_account_info(credentials_info)
+# credentials_json = credentials_json.replace('\\n', '\n')
+# credentials_info = json.loads(credentials_json)
+GS_CREDENTIALS = service_account.Credentials.from_service_account_info(credentials_json)
 
 MEDIA_URL = f'https://storage.googleapis.com/{GS_BUCKET_NAME}/'  # Media URL for serving uploaded files

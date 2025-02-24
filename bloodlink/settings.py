c8dccf3377abebd8593273ccf7b3fb04cabcb8d8
ShouldSleep8hr <pearanuban@gmail.com>
2025-02-24 17:37:30 +0700

    set access token to 15min

--- bloodlink/settings.py
+++ bloodlink/settings.py
@@ -260,7 +260,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SIMPLE_JWT = {
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=2),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
 }
 
